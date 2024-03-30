7175f1b550bb5e8ca4ddf946db29ac9de7e923b3
MS Hussain <hussainsaddan786@gmail.com>
2024-03-30 14:25:50 +0530

    Spring Security Project

---
+++ src/main/java/in/hussaincode/security/book/BookRepository.java
@@ -0,0 +1,6 @@
+package in.hussaincode.security.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+}
---
+++ src/main/java/in/hussaincode/security/token/TokenType.java
@@ -0,0 +1,5 @@
+package in.hussaincode.security.token;
+
+public enum TokenType {
+    BEARER
+}
